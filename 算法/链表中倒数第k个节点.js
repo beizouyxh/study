1748c4d7e929df9032285fe1cccf7ad3f5e3774f
beizouyxh <874609947@qq.com>
2021-03-18 22:53:35 +0800

    function ListNode(val) {

---
+++ 算法/链表中倒数第k个节点.js
@@ -0,0 +1,18 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var getKthFromEnd = function(head, k) {
+  let nodeLength =  0;
+  let headI = head;
+  let nodeMap = new Map();
+  while(headI !==null){
+      nodeMap.set(nodeLength,headI);
+      headI = headI.next;
+      nodeLength++;
+  }
+  return nodeMap.get(nodeLength-k);
+};
+
+getKthFromEnd([1,2,3,4,6],2)

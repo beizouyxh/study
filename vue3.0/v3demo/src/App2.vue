15be0ae7ae4b2013ec8387a8ea46074a6b668b20
beizouyxh <874609947@qq.com>
2021-04-02 21:11:24 +0800

    vue3.0 demo

---
+++ vue3.0/v3demo/src/App2.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  <form>
+    <input type="text" v-model="stu.id">
+    <input type="text" v-model="stu.name">
+    <input type="text" v-model="stu.age">
+    <input type="submit" @click="addStu">
+  </form>
+  <ul>
+    <li v-for="(stu,index) in stus" :key="stus.id" @click="removeStu(index)">
+    {{stu.name}} -- {{stu.age}}
+  </li>
+
+  </ul>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data:function(){
+    return{
+      stus:[
+        {id:1,name:"ss",age:10},
+        {id:2,name:"xx",age:100},
+        {id:3,name:"cc",age:1000},
+      ],
+      stu:{
+        id:'',
+        name:'',
+        age:'',
+      }
+    }
+  },
+  methods:{
+    removeStu(index){
+      this.stus= this.stus.filter((stu,idx) => idx!==index);
+    },
+    addStu(e){
+      e.preventDefault();
+      const stu = Object.assign({},this.stu);
+      this.stus.push(stu);
+      this.stu.id='';
+      this.stu.name='';
+      this.stu.age='';
+    }
+  }
+}
+</script>
---
+++ vue3.0/v3demo/src/App3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{count}}</div>
+  <button @click="myFn">按钮</button>
+</template>
+<script>
+import {ref} from 'vue';
+
+export default{
+  name:'App3',
+  setup() {
+    let count = ref(0);
+    function myFn(){
+     count.value +=1;
+    }
+    return{count,myFn}
+  },
+}
+</script>
+<style scoped>
+
+</style>
